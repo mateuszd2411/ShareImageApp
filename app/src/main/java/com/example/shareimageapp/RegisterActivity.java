15d6672ba1f669448d40cde39359e199bfd9bdb7
mateuszd2411 <mateuszd2411@gmail.com>
2020-01-23 13:39:35 +0100

    Added colors, LoginActivity, RegisterActivity, StartActivity. Design for: activity_login.xml, activity_register.xml. Set views in RegisterActivity, txt_login (have account) set clickable (go to LoginActivity)

---
+++ app/src/main/java/com/example/shareimageapp/RegisterActivity.java
@@ -0,0 +1,54 @@
+package com.example.shareimageapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, fullname, email, password;
+    Button register;
+    TextView txt_login;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        txt_login = findViewById(R.id.txt_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
