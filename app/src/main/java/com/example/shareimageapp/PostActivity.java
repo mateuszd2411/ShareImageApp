89520b92cd578d80e51160a6d706f20be47dcad6
mateuszd2411 <mateuszd2411@gmail.com>
2020-03-07 12:07:34 +0100

    Init views in PostActivity and set clickable close and post buttons

--- app/src/main/java/com/example/shareimageapp/PostActivity.java
+++ app/src/main/java/com/example/shareimageapp/PostActivity.java
@@ -2,13 +2,60 @@ package com.example.shareimageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 
 public class PostActivity extends AppCompatActivity {
+
+    Uri imageUri;
+    String myUrl;
+    StorageTask uploadTask;
+    StorageReference storageReference;
+
+    //views from activity_post.xml
+    ImageView close, image_added;
+    TextView post;
+    EditText description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        //init views
+        close = findViewById(R.id.close);
+        image_added = findViewById(R.id.image_added);
+        post = findViewById(R.id.post);
+        description = findViewById(R.id.description);
+
+        //create folder "Posts" in FirebaseStorage
+        storageReference = FirebaseStorage.getInstance().getReference("Posts");
+
+        //clickable close button, go to MainActivity
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PostActivity.this, MainActivity.class));
+                finish();   //don't back
+            }
+        });
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
