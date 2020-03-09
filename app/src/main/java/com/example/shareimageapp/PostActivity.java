222dc575d2fb48f26ebb1451ef12f2ba066b1d56
mateuszd2411 <mateuszd2411@gmail.com>
2020-03-09 11:31:21 +0100

    Added logic for upload image and image info to database

--- app/src/main/java/com/example/shareimageapp/PostActivity.java
+++ app/src/main/java/com/example/shareimageapp/PostActivity.java
@@ -1,8 +1,10 @@
 package com.example.shareimageapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
@@ -14,10 +16,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -75,6 +86,68 @@ public class PostActivity extends AppCompatActivity {
     }
 
     private void uploadImage() {
+        //progress dialog while posting
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Posting");
+        progressDialog.show();
+
+        //logic for posting
+        if (imageUri != null) {
+            final StorageReference filereference = storageReference.child(System.currentTimeMillis()
+                    + "." + getFileExtension(imageUri));
+
+            uploadTask = filereference.putFile(imageUri);
+            uploadTask.continueWith(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return filereference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    //when all is good
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = (Uri) task.getResult();
+                        myUrl = downloadUri.toString();
+
+                        //go to/make folder i realtime database
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+                        //get key for make postid
+                        String postid = reference.push().getKey();
+
+                        //put post info to realtime database
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("postid", postid);
+                        hashMap.put("postimage", myUrl);
+                        hashMap.put("description", description.getText().toString());
+                        hashMap.put("publisher", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                        reference.child(postid).setValue(hashMap);
+
+                        progressDialog.dismiss();
+
+                        //after all go to MainActivity
+                        startActivity(new Intent(PostActivity.this, MainActivity.class));
+                        finish();   //don't go back
+                    } else {
+                        //display message when something wrong
+                        Toast.makeText(PostActivity.this, "Failed...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    //something wrong with realtime database
+                    Toast.makeText(PostActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(this, "No Image selected...", Toast.LENGTH_SHORT).show();
+        } //logic for posting END
 
     }
 
