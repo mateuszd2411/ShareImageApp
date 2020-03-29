4e673b780ad29489586f13406642bed643d6617a
mateuszd2411 <mateuszd2411@gmail.com>
2020-03-29 11:07:46 +0200

    Added Post Model and constructors, getters and setters: postid, postimage, description, publisher

---
+++ app/src/main/java/com/example/shareimageapp/Model/Post.java
@@ -0,0 +1,54 @@
+package com.example.shareimageapp.Model;
+
+public class Post {
+    private String postid;
+    private String postimage;
+    private String description;
+    private String publisher;
+
+    public Post(String postid, String postimage, String description, String publisher) {
+        this.postid = postid;
+        this.postimage = postimage;
+        this.description = description;
+        this.publisher = publisher;
+    }
+
+    //empty constructor for database
+    public Post() {
+    }
+
+
+    //getters and setters
+
+    public String getPostid() {
+        return postid;
+    }
+
+    public void setPostid(String postid) {
+        this.postid = postid;
+    }
+
+    public String getPostimage() {
+        return postimage;
+    }
+
+    public void setPostimage(String postimage) {
+        this.postimage = postimage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
