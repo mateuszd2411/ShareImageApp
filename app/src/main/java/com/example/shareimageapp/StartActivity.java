694f20d83103c944a970f80c24a2e7f27d6ce80e
mateuszd2411 <mateuszd2411@gmail.com>
2020-04-01 14:27:52 +0200

    In StartActivity added logic for choosing and change language

--- app/src/main/java/com/example/shareimageapp/StartActivity.java
+++ app/src/main/java/com/example/shareimageapp/StartActivity.java
@@ -1,8 +1,13 @@
 package com.example.shareimageapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,9 +15,11 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Locale;
+
 public class StartActivity extends AppCompatActivity {
 
-    Button login, register;
+    Button login, register, changeLang;
 
     FirebaseUser firebaseUser;
 
@@ -32,10 +39,12 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();   //for language
         setContentView(R.layout.activity_start);
 
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
+        changeLang = findViewById(R.id.change_anguage);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +59,64 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(new Intent(StartActivity.this, RegisterActivity.class));
             }
         });
+
+        //For change language
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //show alert dialog to display list of language, one can be selected
+                showChangeLanguageDialog();
+            }
+        });
+    }// onCreate END
+
+    //For change language
+    private void showChangeLanguageDialog() {
+        //array of language to display in alert dialog
+        final String[] listItems = {"Polski", "English"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(StartActivity.this);
+        builder.setTitle("Choose Language...");
+        builder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0) {
+                    //Polski
+                    setLocale("pl");
+                    recreate();
+                }
+                if (i == 1) {
+                    //English
+                    setLocale("en");
+                    recreate();
+                }
+
+                //dismiss alert dialog when language selected
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog mDialog = builder.create();
+        builder.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+
+        //save data to share preferences
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    //load language saved in share preferences
+    private void loadLocale() {
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
     }
 }
