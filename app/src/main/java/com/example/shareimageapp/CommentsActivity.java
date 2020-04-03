8d92493e7d53b6ac979abc08df46e476b717db6c
mateuszd2411 <mateuszd2411@gmail.com>
2020-04-03 16:38:18 +0200

    CommentsActivity: Toolbar, getStringExtra: postid, publisherid for individual user post. Validation for post/send comment button

--- app/src/main/java/com/example/shareimageapp/CommentsActivity.java
+++ app/src/main/java/com/example/shareimageapp/CommentsActivity.java
@@ -2,17 +2,30 @@ package com.example.shareimageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+
+import es.dmoral.toasty.Toasty;
 
 public class CommentsActivity extends AppCompatActivity {
 
     EditText addcomment;
     ImageView image_profile;
     TextView post;
+
+    String postid;
+    String publisherid;
+
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +41,40 @@ public class CommentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
 
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.Comments);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         addcomment = findViewById(R.id.add_comment);
         image_profile = findViewById(R.id.image_profile);
         post = findViewById(R.id.post);
+
+        //need postid, publisherid
+        Intent intent = getIntent();
+        postid = intent.getStringExtra("postid");
+        publisherid = intent.getStringExtra("publisherid");
+
+        //post/send comment button clickable
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //validation if not empty comment
+                if (addcomment.getText().toString().equals("")){
+                    //Toast info
+                    Toasty.info(CommentsActivity.this, R.string.canysendemptycomment, Toast.LENGTH_LONG, true).show();
+                }else {
+                    //add comment logic
+                }
+            }
+        });
     }
 }
