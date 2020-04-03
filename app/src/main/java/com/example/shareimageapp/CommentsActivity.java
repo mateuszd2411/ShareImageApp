8bf0a48dbdae984d33afec228723483415b44d82
mateuszd2411 <mateuszd2411@gmail.com>
2020-04-03 16:28:51 +0200

    In CommentsActivity.java init views and added logic for dark/day theme mode

--- app/src/main/java/com/example/shareimageapp/CommentsActivity.java
+++ app/src/main/java/com/example/shareimageapp/CommentsActivity.java
@@ -1,14 +1,35 @@
 package com.example.shareimageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CommentsActivity extends AppCompatActivity {
 
+    EditText addcomment;
+    ImageView image_profile;
+    TextView post;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ////////////////////////////////////////////For Dark Theme
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darkTheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+
+        ////////////////////////////////////////////For Dark Theme
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        addcomment = findViewById(R.id.add_comment);
+        image_profile = findViewById(R.id.image_profile);
+        post = findViewById(R.id.post);
     }
 }
