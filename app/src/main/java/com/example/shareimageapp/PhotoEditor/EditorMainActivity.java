521d7ffcc3f46761c6728eb13a54a5c33ffe183c
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-15 06:18:09 +0200

    Start Photo Editor added dependencies for permissions (Dexter) and filters androidhive:imagefilters. Added EditorMainActivity and content_main_for_editor.xml

---
+++ app/src/main/java/com/example/shareimageapp/PhotoEditor/EditorMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.shareimageapp.PhotoEditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shareimageapp.R;
+
+public class EditorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor_main);
+    }
+}
