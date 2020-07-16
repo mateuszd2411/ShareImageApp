ab245b989572da79620f66c7dda498683bbffe18
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-16 07:30:41 +0200

    Added Adapters: ThumbnailAdapter and ViewPagerAdapter, colors for touch filter

--- app/src/main/java/com/example/shareimageapp/PhotoEditor/Adapter/ThumbnailAdapter.java
+++ app/src/main/java/com/example/shareimageapp/PhotoEditor/Adapter/ThumbnailAdapter.java
@@ -1,31 +1,71 @@
 package com.example.shareimageapp.PhotoEditor.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.shareimageapp.PhotoEditor.Interface.FiltersListFragmentListener;
 import com.example.shareimageapp.R;
+import com.zomato.photofilters.utils.ThumbnailItem;
+
+import java.util.List;
 
 public class ThumbnailAdapter extends RecyclerView.Adapter<ThumbnailAdapter.MyViewHolder> {
+
+    private List<ThumbnailItem> thumbnailItems;
+    private FiltersListFragmentListener listener;
+    private Context context;
+    private int selectedIndex = 0;
+
+    public ThumbnailAdapter(List<ThumbnailItem> thumbnailItems, FiltersListFragmentListener listener, Context context) {
+        this.thumbnailItems = thumbnailItems;
+        this.listener = listener;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(context).inflate(R.layout.thumbnail_item, parent, false);
+
+        return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        final ThumbnailItem thumbnailItem = thumbnailItems.get(position);
 
+        holder.thumbnail.setImageBitmap(thumbnailItem.image);
+        //click listener
+        holder.thumbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onFilterSelected(thumbnailItem.filter);
+                selectedIndex = position;
+
+                notifyDataSetChanged();
+            }
+        });
+
+        holder.filter_name.setText(thumbnailItem.filterName);
+
+        if (selectedIndex == position) {
+            holder.filter_name.setTextColor(ContextCompat.getColor(context, R.color.selected_filter));
+        } else {
+            holder.filter_name.setTextColor(ContextCompat.getColor(context, R.color.normal_filter));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return thumbnailItems.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
