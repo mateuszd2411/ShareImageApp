6368f201f2339f73c5c61572fcaf7f13a477fa8b
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-16 07:59:43 +0200

    Added flash.jpg default image to edit, in FilterListFragment set displayThumbnail(), in EditorMainActivity set filters

--- app/src/main/java/com/example/shareimageapp/PhotoEditor/EditorMainActivity.java
+++ app/src/main/java/com/example/shareimageapp/PhotoEditor/EditorMainActivity.java
@@ -1,16 +1,146 @@
 package com.example.shareimageapp.PhotoEditor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.example.shareimageapp.PhotoEditor.Adapter.ViewPagerAdapter;
+import com.example.shareimageapp.PhotoEditor.Interface.EditImageFragmentListener;
+import com.example.shareimageapp.PhotoEditor.Interface.FiltersListFragmentListener;
+import com.example.shareimageapp.PhotoEditor.Utils.BitmapUtils;
 import com.example.shareimageapp.R;
+import com.google.android.material.tabs.TabLayout;
+import com.zomato.photofilters.imageprocessors.Filter;
+import com.zomato.photofilters.imageprocessors.subfilters.BrightnessSubFilter;
+import com.zomato.photofilters.imageprocessors.subfilters.ContrastSubFilter;
+import com.zomato.photofilters.imageprocessors.subfilters.SaturationSubfilter;
 
-public class EditorMainActivity extends AppCompatActivity {
+public class EditorMainActivity extends AppCompatActivity implements FiltersListFragmentListener, EditImageFragmentListener {
+
+    public static final String pictureName = "flash.jpg";
+
+    public static final int PERMISSION_PICK_IMAGE = 1000;
+
+    ImageView img_preview;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    CoordinatorLayout coordinatorLayout;
+
+    Bitmap originalBitmap, filteredBitmap, finalBitmap;
+
+    FilterListFragment filtersListFragment;
+    EditImageFragment editImageFragment;
+
+    int brightnessFinal = 0;
+    float saturationFinal = 1.0f;
+    float contrastFinal = 1.0f;
+
+    //Load native image filters lib
+    static {
+        System.loadLibrary("NativeImageProcessor");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor_main);
+//        Toolbar toolbar = findViewById(R.id.toolBar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setTitle("Instagram Filter");
+
+        //view
+        img_preview = (ImageView) findViewById(R.id.image_preview);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator);
+
+        loadImage();
+
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void loadImage() {
+        originalBitmap = BitmapUtils.getBitmapFromAssets(this, pictureName, 300, 300);
+        filteredBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        finalBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        img_preview.setImageBitmap(originalBitmap);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        filtersListFragment = new FilterListFragment();
+        filtersListFragment.setListener(this);
+
+        editImageFragment = new EditImageFragment();
+        editImageFragment.setListener(this);
+
+        adapter.addFragment(filtersListFragment, "FILTERS");
+        adapter.addFragment(editImageFragment, "EDIT");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBrightnessChanged(int brightness) {
+        brightnessFinal = brightness;
+        Filter myFilter = new Filter();
+        myFilter.addSubFilter(new BrightnessSubFilter(brightness));
+        img_preview.setImageBitmap(myFilter.processFilter(finalBitmap.copy(Bitmap.Config.ARGB_8888,true)));
+    }
+
+    @Override
+    public void onSaturationChanged(float saturation) {
+        saturationFinal = saturation;
+        Filter myFilter = new Filter();
+        myFilter.addSubFilter(new SaturationSubfilter(saturation));
+        img_preview.setImageBitmap(myFilter.processFilter(finalBitmap.copy(Bitmap.Config.ARGB_8888,true)));
+    }
+
+    @Override
+    public void onContrastChanged(float contrast) {
+        contrastFinal = contrast;
+        Filter myFilter = new Filter();
+        myFilter.addSubFilter(new ContrastSubFilter(contrast));
+        img_preview.setImageBitmap(myFilter.processFilter(finalBitmap.copy(Bitmap.Config.ARGB_8888,true)));
+    }
+
+    @Override
+    public void onEditStarted() {
+
+    }
+
+    @Override
+    public void onEditCompleted() {
+        Bitmap bitmap = filteredBitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        Filter myFilter = new Filter();
+        myFilter.addSubFilter(new BrightnessSubFilter(brightnessFinal));
+        myFilter.addSubFilter(new SaturationSubfilter(saturationFinal));
+        myFilter.addSubFilter(new ContrastSubFilter(contrastFinal));
+
+        finalBitmap = myFilter.processFilter(bitmap);
+    }
+
+    @Override
+    public void onFilterSelected(Filter filter) {
+        resetControl();
+        filteredBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        img_preview.setImageBitmap(filter.processFilter(filteredBitmap));
+        finalBitmap = filteredBitmap.copy(Bitmap.Config.ARGB_8888, true);
+    }
+
+    private void resetControl() {
+        if (editImageFragment != null)
+            editImageFragment.resetControls();
+        brightnessFinal = 0;
+        saturationFinal = 1.0f;
+        contrastFinal = 1.0f;
     }
 }
--- app/src/main/java/com/example/shareimageapp/PhotoEditor/FilterListFragment.java
+++ app/src/main/java/com/example/shareimageapp/PhotoEditor/FilterListFragment.java
@@ -1,20 +1,51 @@
 package com.example.shareimageapp.PhotoEditor;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.shareimageapp.PhotoEditor.Adapter.ThumbnailAdapter;
+import com.example.shareimageapp.PhotoEditor.Interface.FiltersListFragmentListener;
+import com.example.shareimageapp.PhotoEditor.Utils.BitmapUtils;
+import com.example.shareimageapp.PhotoEditor.Utils.SpacesItemDecoration;
 import com.example.shareimageapp.R;
+import com.zomato.photofilters.FilterPack;
+import com.zomato.photofilters.imageprocessors.Filter;
+import com.zomato.photofilters.utils.ThumbnailItem;
+import com.zomato.photofilters.utils.ThumbnailsManager;
 
-public class FilterListFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class FilterListFragment extends Fragment implements FiltersListFragmentListener {
+
+    RecyclerView recyclerView;
+    ThumbnailAdapter adapter;
+    List<ThumbnailItem> thumbnailItems;
+
+    FiltersListFragmentListener listener;
+
+    public void setListener(FiltersListFragmentListener listener) {
+        this.listener = listener;
+    }
 
     public FilterListFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
 
@@ -22,6 +53,78 @@ public class FilterListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter_list, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_filter_list, container, false);
+
+        thumbnailItems = new ArrayList<>();
+        adapter = new ThumbnailAdapter(thumbnailItems, this, getActivity());
+
+        //recycler view
+        recyclerView = (RecyclerView) itemView.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        int space = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8,
+                getResources().getDisplayMetrics());
+        recyclerView.addItemDecoration(new SpacesItemDecoration(space));
+        recyclerView.setAdapter(adapter);
+
+        displayThumbnail(null);
+
+        return itemView;
+    }
+
+    private void displayThumbnail(final Bitmap bitmap) {
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Bitmap thumbImg;
+                if (bitmap == null) {
+                    thumbImg = BitmapUtils.getBitmapFromAssets(getActivity(),
+                            EditorMainActivity.pictureName, 100, 100);
+                } else {
+                    thumbImg = Bitmap.createScaledBitmap(bitmap, 100, 100, false);
+                }
+
+                if (thumbImg == null)
+                    return;
+                ThumbnailsManager.clearThumbs();
+                thumbnailItems.clear();
+
+                //add normal bitmap first
+                ThumbnailItem thumbnailItem = new ThumbnailItem();
+                thumbnailItem.image = thumbImg;
+                thumbnailItem.filterName = "Normal";
+                ThumbnailsManager.addThumb(thumbnailItem);
+
+                List<Filter> filters = FilterPack.getFilterPack(getActivity());
+
+                for (Filter filter : filters) {
+                    ThumbnailItem tI = new ThumbnailItem();
+                    tI.image = thumbImg;
+                    tI.filter = filter;
+                    tI.filterName = filter.getName();
+                    ThumbnailsManager.addThumb(tI);
+                }
+
+                thumbnailItems.addAll(ThumbnailsManager.processThumbs(getActivity()));
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+            }
+        };
+        new Thread(r).start();
+    }
+
+    public void onFilterSelected(Filter filter) {
+        if (listener != null) {
+            listener.onFilterSelected(filter);
+        }
     }
 }
