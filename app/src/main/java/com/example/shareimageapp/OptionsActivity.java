3a7bed28ff50817dd19b9c42078524989c677101
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-25 08:12:36 +0200

    In OptionsActivity init views  set clickable logout button. Added progress bar to home fragment

--- app/src/main/java/com/example/shareimageapp/OptionsActivity.java
+++ app/src/main/java/com/example/shareimageapp/OptionsActivity.java
@@ -1,14 +1,54 @@
 package com.example.shareimageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class OptionsActivity extends AppCompatActivity {
+
+    @BindView(R.id.logout)
+    TextView logout;
+    @BindView(R.id.settings)
+    TextView settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        //bind the current view
+        ButterKnife.bind(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.options);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(OptionsActivity.this, StartActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
     }
 }
